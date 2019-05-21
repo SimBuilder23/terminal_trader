eb52ecdcae33ce6b93f7b8b8c7477379a6db3895
Kyle Rippere <kylerippere@gmail.com>
2019-05-21 14:34:20 -0400

    add get_quote function

--- wrapper.py
+++ wrapper.py
@@ -10,12 +10,15 @@ def lookup(company_name):
     ticker_symbol = response[0]['Symbol']
     return ticker_symbol
 
-def quote():
-    pass
-
+def quote(ticker_symbol):
+    response = json.loads(requests.get(f'http://dev.markitondemand.com/MODApis/Api/v2/quote/json?symbol={ticker_symbol}').text)
+    quote = response['LastPrice']
+    return quote
 
 if __name__ == "__main__":
-    #response = json.loads(requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=TSLA').text)
-    user_input = input("What ticker symbol?")
-    print (lookup(user_input))
+ #   response = json.loads(requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=TSLA').text)
+ #   user_input = input("What company name? ")
+ #   print (lookup(user_input))
 
+    user_input = input ("Which ticker symbol? ")
+    print (quote(user_input))
